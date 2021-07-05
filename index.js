0d8a91e9b0da8848969c8936ac91bc26422e5993
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-07-06 06:03:01 +0800

    Part2 - Routes

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+require("./routes/dialogFlowRoutes")(app);
 
 app.get("/", (req, res) => {
   res.send({ hi: "awaw" });
