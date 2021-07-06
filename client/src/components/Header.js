f62b5518ee9a47b20a99822d59463c4dd9874ed9
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-07-07 03:59:54 +0800

    Part3 - Components for the page - part 2

---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to={"/shop"}>Shop</Link>
+      </li>
+      <li>
+        <Link to={"/about"}>About us</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Header;
--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>Selling you great stuff!</h1>
       with the help of the chatbot
     </div>
