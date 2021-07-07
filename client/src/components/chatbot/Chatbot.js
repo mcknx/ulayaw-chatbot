388e16c31ea3118bf274cc4ae92e3b4afc125d09
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-07-08 02:22:15 +0800

    Part3 - Connect-Server-and-Client

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Chatbot = () => <h2>Chatbot will be here</h2>;
+function Chatbot() {
+  const [messages, setMessages] = useState();
+  return (
+    <div className="h-96 w-64 float-right border-2 ">
+      <div className="w-full h-full overflow-auto">
+        <h2>Chatbot</h2>
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
+
+// const Chatbot = () => <h2>Chatbot will be here</h2>;
 
 export default Chatbot;
