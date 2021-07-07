45ec0da4fe3029bb7b0a5052eaafbdc09b033ea7
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-07-08 02:42:37 +0800

    Part3 - Add API calls to backend APP

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,7 +1,42 @@
 import React, { useState } from "react";
+import axios from "axios/index";
 
 function Chatbot() {
   const [messages, setMessages] = useState();
+
+  async function df_text_query(text) {
+    let says = {
+      speaks: "me",
+      msg: {
+        text: {
+          text: text,
+        },
+      },
+    };
+    setMessages({ messages: [...messages, says] });
+    const res = await axios.post("/api/df_text_query", { text });
+
+    for (let msg of res.data.fulfillmentMessages) {
+      says = {
+        speaks: "bot",
+        msg: msg,
+      };
+      setMessages({ messages: [...messages, says] });
+    }
+  }
+
+  async function df_event_query(event) {
+    const res = await axios.post("/api/df_event_query", { event });
+
+    for (let msg of res.data.fulfillmentMessages) {
+      says = {
+        speaks: "me",
+        msg: msg,
+      };
+      setMessages({ messages: [...messages, says] });
+    }
+  }
+
   return (
     <div className="h-96 w-64 float-right border-2 ">
       <div className="w-full h-full overflow-auto">
