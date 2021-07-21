2f85877cb629369ceab54e02198ac3bb97a7ab43
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-07-21 09:39:20 +0800

    Part7 - FrontEnd routing on Server

--- index.js
+++ index.js
@@ -26,5 +26,16 @@ require("./routes/fulfillmentRoutes")(app);
 app.get("/", (req, res) => {
   res.send({ hi: "awawawawas" });
 });
+
+if (process.env.NODE_ENV === "production") {
+  // js and css files
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
