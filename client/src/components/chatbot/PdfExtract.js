4a5483b348b17fbb7b85bc5e9ecf5fc54845b938
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-12-04 04:07:42 +0800

    pdf build

--- client/src/components/chatbot/PdfExtract.js
+++ client/src/components/chatbot/PdfExtract.js
@@ -1,42 +1,42 @@
-import React from "react";
-import {
-  Document,
-  Page,
-  View,
-  Text,
-  Link,
-  Font,
-  StyleSheet,
-} from "@react-pdf/renderer";
+// import React from "react";
+// import {
+//   Document,
+//   Page,
+//   View,
+//   Text,
+//   Link,
+//   Font,
+//   StyleSheet,
+// } from "@react-pdf/renderer";
 
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-});
+// const styles = StyleSheet.create({
+//   page: {
+//     flexDirection: "row",
+//     backgroundColor: "#E4E4E4",
+//   },
+//   section: {
+//     margin: 10,
+//     padding: 10,
+//     flexGrow: 1,
+//   },
+// });
 
-Font.register({
-  family: "Oswald",
-  src: "https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf",
-});
+// Font.register({
+//   family: "Oswald",
+//   src: "https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf",
+// });
 
-const PdfExtract = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-);
+// const PdfExtract = () => (
+//   <Document>
+//     <Page size="A4" style={styles.page}>
+//       <View style={styles.section}>
+//         <Text>Section #1</Text>
+//       </View>
+//       <View style={styles.section}>
+//         <Text>Section #2</Text>
+//       </View>
+//     </Page>
+//   </Document>
+// );
 
-export default PdfExtract;
+// export default PdfExtract;
