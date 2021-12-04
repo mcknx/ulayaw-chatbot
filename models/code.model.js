8f2fc8912accad04f11776771c06c3538c89a195
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-12-04 21:20:11 +0800

    code model done

--- models/code.model.js
+++ models/code.model.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 // user schema
 const codeModel = new mongoose.Schema(
   {
-    admin_id: { type: String, default: "1" },
-    user_id: { type: String, default: "1" },
-    admin_first_name: { type: String, default: "Mckeen" },
-    admin_last_name: { type: String, default: "Asma" },
+    admin_id: { type: Boolean, default: false },
+    user_id: { type: Boolean, default: false },
+    admin_first_name: { type: String, default: "" },
+    admin_last_name: { type: String, default: "" },
     admin_contact_no: { type: String, default: "" },
-    admin_email: { type: String, default: "mckeenasma123@gmail.com" },
-    user_email: { type: String, default: "mckeenasmarvmz@gmail.com" },
+    admin_email: { type: String, default: "" },
+    user_email: { type: String, default: "" },
     code: { type: String, default: "" },
-    companion: { type: Object, default: {} },
+    companion: { type: Object, trim: true },
+    result: { type: String, default: "" },
   },
   {
     timestamps: true,
