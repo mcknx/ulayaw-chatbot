5a73b8224067c2a05b3d74236f36dcb3fbd450f1
mckeenasma <50982785+mckeenasma@users.noreply.github.com>
2021-12-04 21:11:28 +0800

    ModalLogin Done

--- client/src/components/ModalLogin.js
+++ client/src/components/ModalLogin.js
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { GoogleLogin } from "react-google-login";
+import { ShowAdminRoute } from "../Context/ShowAdminRoute";
 import { FaGoogle } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
 import { authenticate, isAuth } from "../helpers/auth";
 import { Link, Redirect } from "react-router-dom";
+import Admin from "./Admin";
 import asdfjkl from "asdfjkl";
 // import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input/input";
 
 export default function ModalLogin(props) {
+  const { showAdminRoute, setShowAdminRoute } = useContext(ShowAdminRoute);
   const [showRegister, setShowRegister] = useState(false);
   const [selectedGender, setSelectedGender] = useState(false);
   const [value, setValue] = useState();
@@ -61,12 +64,62 @@ export default function ModalLogin(props) {
     }
   };
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     if (props.assessment_meron_companion) {
-      props.setassessment_meron_companion(false);
-      props.setShowModal(false);
-      toast.success("Successfully added Companion!");
+      props.setAssessmentUser((prevAss) => {
+        return {
+          ...prevAss,
+          companion: {
+            companion_first_name: formData.first_name,
+            companion_last_name: formData.last_name,
+            companion_contact_no: formData.contact_no,
+          },
+        };
+      });
+      let assessmentTaker = props.assessmentUser;
+      await axios
+        .post(`/api/admin/addCompanion`, {
+          assessmentTaker,
+          companion_first_name: formData.first_name,
+          companion_last_name: formData.last_name,
+          companion_contact_no: formData.contact_no,
+        })
+        .then((res) => {
+          // toast.success(res.data.message);
+          props.setassessment_meron_companion(false);
+          props.setassessment_meron_companion_done(false);
+          props.setShowModal(false);
+          toast.success("Successfully added Companion!");
+          props._handleTranslate(`I have a companion`, `Meron ka kasama`, true);
+          props._handleTranslate(
+            `Nais kong panatilihing mo makiugnay kay ${formData.first_name}. Maaari mo siyang kausapin o bigyan ng indikasyon na-aayon sa iyong nararamdaman o naiisip.`,
+            `Nais kong panatilihing mo makiugnay kay ${formData.first_name}. Maaari mo siyang kausapin o bigyan ng indikasyon na-aayon sa iyong nararamdaman o naiisip.`
+          );
+          props._handleTranslate(
+            `Salamat sa pagkakataong ibinigay mo upang mapag usapan ang iyong mga suliranin. Base sa ating mga napag usapan, mas makatutulong na ipagpatuloy ang pagproseso ng iyong concerns sa pamamagitan ng psychiatric consultation. Huwag ka sanang mahihiyang lumapit muli sa akin kapag kailangan mo ulit ng aming tulong. Palagi kang mag iingat!`,
+            `Salamat sa pagkakataong ibinigay mo upang mapag usapan ang iyong mga suliranin. Base sa ating mga napag usapan, mas makatutulong na ipagpatuloy ang pagproseso ng iyong concerns sa pamamagitan ng psychiatric consultation. Huwag ka sanang mahihiyang lumapit muli sa akin kapag kailangan mo ulit ng aming tulong. Palagi kang mag iingat!`
+          );
+
+          props._handleTranslate(
+            `Nais mo na bang ipagpatuloy ang pakikipag usap natin? May ibabahagi sana akong tool sa iyo.`,
+            `Nais mo na bang ipagpatuloy ang pakikipag usap natin? May ibabahagi sana akong tool sa iyo.`
+          );
+          props._handleTranslate(
+            `Ito ay ginagamit ko paminsan minsan upang ma bawasan ang bigat na aking nararamdaman. 🤗`,
+            `Ito ay ginagamit ko paminsan minsan upang ma bawasan ang bigat na aking nararamdaman. 🤗`
+          );
+          props.df_event_query("ASSESSMENT_DONE");
+          // setCorrectCode(true);
+          // console.log(res.data.user);
+          // setAssessmentUser(res.data.user);
+          // toast.success(res.data.message);
+        })
+        .catch((err) => {
+          // setCorrectCode(false);
+          // console.log(err.response);
+          toast.error(err.response.data.errors);
+        });
     } else {
       if (showRegister) {
         // Register
@@ -177,26 +230,31 @@ export default function ModalLogin(props) {
                 // isAuth() && isAuth().role === "admin"
                 //   ? props.history.push("/admin")
                 //   : props.history.push("/client");
-                toast.success(`Hey ${res.data.user.first_name}, Welcome!`);
-                props.setUserLoggedIn(res.data.user);
+                console.log(isAuth());
+                if (isAuth() && isAuth().role === "admin") {
+                  setShowAdminRoute(true);
+                } else {
+                  toast.success(`Hey ${res.data.user.first_name}, Welcome!`);
+                  props.setUserLoggedIn(res.data.user);
 
-                props._handleTranslate(`Login`, `Login`, true);
-                props._handleTranslate(
-                  `We appreciate your signing in, ${res.data.user.first_name}. We've verified that you're ${res.data.user.age} years old and that you're a ${res.data.user.gender}.`,
-                  `Hello ${res.data.user.first_name}, nagpapasalamat kami sa iyong pag sign in. Napag alaman namin na ikaw ay ${res.data.user.age} taong gulang at isang ${res.data.user.gender}.`
-                );
-                props._handleTranslate(
-                  `The information will be included in your profile's documentation. You can be assured that your data will be kept confidential and secure while in our care.`,
-                  `Ang mga impormasyong ito ay magiging bahagi lamang Sa dokumentasyon para sa iyong propayl. Makasisigurado po kayo na ang mga ito ay mananatiling pribado at ligtas sa aming pangangalaga.`
-                );
+                  props._handleTranslate(`Login`, `Login`, true);
+                  props._handleTranslate(
+                    `We appreciate your signing in, ${res.data.user.first_name}. We've verified that you're ${res.data.user.age} years old and that you're a ${res.data.user.gender}.`,
+                    `Hello ${res.data.user.first_name}, nagpapasalamat kami sa iyong pag sign in. Napag alaman namin na ikaw ay ${res.data.user.age} taong gulang at isang ${res.data.user.gender}.`
+                  );
+                  props._handleTranslate(
+                    `The information will be included in your profile's documentation. You can be assured that your data will be kept confidential and secure while in our care.`,
+                    `Ang mga impormasyong ito ay magiging bahagi lamang Sa dokumentasyon para sa iyong propayl. Makasisigurado po kayo na ang mga ito ay mananatiling pribado at ligtas sa aming pangangalaga.`
+                  );
 
-                props._handleTranslate(
-                  `Do you want to start our conversation? 😳 or you have an Assessment Code from the PMHA?`,
-                  `Gusto mo na bang mag simula? 😳 o mayroon kang Assessment Code mula sa PMHA?.`
-                );
+                  props._handleTranslate(
+                    `Do you want to start our conversation? 😳 or you have an Assessment Code from the PMHA?`,
+                    `Gusto mo na bang mag simula? 😳 o mayroon kang Assessment Code mula sa PMHA?.`
+                  );
 
-                props.df_event_query("LOGIN_CONTINUE");
-                props.setShowModal(false);
+                  props.df_event_query("LOGIN_CONTINUE");
+                  props.setShowModal(false);
+                }
               });
             })
             .catch((err) => {
@@ -220,9 +278,15 @@ export default function ModalLogin(props) {
 
   const responseGoogle = (response) => {
     console.log(response);
-    if (response.profileObj.email === "masma_180000002118@uic.edu.ph") {
+    if (
+      response.profileObj.email === "masma_180000002118@uic.edu.ph" ||
+      response.profileObj.email === "ebayacag_180000002988@uic.edu.ph" ||
+      response.profileObj.email === "ctalo_180000001941@uic.edu.ph" ||
+      response.profileObj.email === "jsurigao_180000002050@uic.edu.ph"
+    ) {
       sendGoogleToken(response.tokenId);
     } else {
+      toast.error("Admin does not exist");
       toast.error("Admin with that email does not exist");
     }
   };
@@ -245,12 +309,17 @@ export default function ModalLogin(props) {
         toast.error(error.response.data.error);
       });
   };
-  const informParent = (response) => {
+  const informParent = async (response) => {
     authenticate(response, () => {
       if (isAuth() && isAuth().role === "admin") {
         // props.setShowModal(false);
-        toast.success(isAuth().email);
-        console.log(isAuth().email);
+        // const data = await axios.post('/someApiUrl')
+        setShowAdminRoute(true);
+
+        props.setShowModal(false);
+
+        // return <Admin />;
+        // console.log(isAuth().email);
       } else {
         console.log("client");
       }
@@ -271,6 +340,9 @@ export default function ModalLogin(props) {
 
                 if (props.setQuickRepliesWelcome) {
                   props.setQuickRepliesWelcome(true);
+                }
+                if (props.assessment_meron_companion_done) {
+                  props.setassessment_meron_companion_done(true);
                 }
               }}
             >
@@ -428,31 +500,43 @@ export default function ModalLogin(props) {
               {props.assessment_meron_companion ? (
                 ""
               ) : (
-                <label className="space-x-[10px]  text-gray-600 inline-block p-4  rounded-lg  select-none w-full ">
-                  {/* email */}
-                  <input
-                    className=" focus:ring-1 focus:ring-[#5DCFFF] p-4 outline-none w-full rounded-[15px]  bg-[#F2F3F7]"
-                    type="email"
-                    placeholder="Email address"
-                    onChange={handleChange("email")}
-                    value={email}
-                  />
-                </label>
+                <>
+                  {props.admin ? (
+                    ""
+                  ) : (
+                    <label className="space-x-[10px]  text-gray-600 inline-block p-4  rounded-lg  select-none w-full ">
+                      {/* email */}
+
+                      <input
+                        className=" focus:ring-1 focus:ring-[#5DCFFF] p-4 outline-none w-full rounded-[15px]  bg-[#F2F3F7]"
+                        type="email"
+                        placeholder="Email address"
+                        onChange={handleChange("email")}
+                        value={email}
+                      />
+                    </label>
+                  )}
+                </>
               )}
               {props.assessment_meron_companion ? (
                 ""
               ) : (
                 <span className={showRegister ? "grid grid-cols-2" : ""}>
                   {/* password 1 */}
-                  <label className="space-x-[10px]  text-gray-600 inline-block p-4  rounded-lg  select-none w-full ">
-                    <input
-                      className=" focus:ring-1 focus:ring-[#5DCFFF] p-4 outline-none w-full rounded-[15px]  bg-[#F2F3F7]"
-                      type="password"
-                      placeholder="Password"
-                      onChange={handleChange("password1")}
-                      value={password1}
-                    />
-                  </label>
+
+                  {props.admin ? (
+                    ""
+                  ) : (
+                    <label className="space-x-[10px]  text-gray-600 inline-block p-4  rounded-lg  select-none w-full ">
+                      <input
+                        className=" focus:ring-1 focus:ring-[#5DCFFF] p-4 outline-none w-full rounded-[15px]  bg-[#F2F3F7]"
+                        type="password"
+                        placeholder="Password"
+                        onChange={handleChange("password1")}
+                        value={password1}
+                      />
+                    </label>
+                  )}
                   {/* password 2 */}
                   {!showRegister ? (
                     ""
@@ -471,7 +555,7 @@ export default function ModalLogin(props) {
               )}
             </span>
 
-            {showRegister || props.assessment_meron_companion ? (
+            {showRegister || props.assessment_meron_companion || props.admin ? (
               ""
             ) : (
               <Link
@@ -484,22 +568,24 @@ export default function ModalLogin(props) {
             )}
 
             {/* submit btn */}
-
-            <button
-              className="self-center rounded-[38px] bg-[#5DCFFF] space-x-[10px]  m-8 py-[20px] w-[405px] text-white text-[24px]"
-              onClick={(e) => {
-                // if (showRegister) {
-                handleSubmit(e);
-                // }
-              }}
-            >
-              {props.assessment_meron_companion ? (
-                "SAVE"
-              ) : (
-                <>{showRegister ? "REGISTER" : "LOGIN"}</>
-              )}
-            </button>
-
+            {props.admin ? (
+              ""
+            ) : (
+              <button
+                className="self-center rounded-[38px] bg-[#5DCFFF] space-x-[10px]  m-8 py-[20px] w-[405px] text-white text-[24px]"
+                onClick={(e) => {
+                  // if (showRegister) {
+                  handleSubmit(e);
+                  // }
+                }}
+              >
+                {props.assessment_meron_companion ? (
+                  "SAVE"
+                ) : (
+                  <>{showRegister ? "REGISTER" : "LOGIN"}</>
+                )}
+              </button>
+            )}
             {props.admin ? (
               <button
                 className="self-center rounded-[38px] bg-[#5DCFFF] space-x-[10px]   py-[20px] w-[405px] text-white text-[24px]"
